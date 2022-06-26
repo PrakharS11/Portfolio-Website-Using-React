9bd4a2289199c05e62753394a4813e612b4443d6
Prakhar Srivastava <prakharsahai21@gmail.com>
2022-06-27 01:17:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import "./Fonts.css";
 import PortfolioWrapper from "./Components/portfolio/PortfolioWrapper";
 import { Switch, Route, BrowserRouter, withRouter } from "react-router-dom";
-import DiceGame from "./Components/scarne/DiceGame";
 
 function App() {
   return (
@@ -13,9 +12,6 @@ function App() {
           <Route exact path="/">
             <PortfolioWrapper />
           </Route>
-          <Route exact path="/dice">
-            <DiceGame />
-          </Route>
         </Switch>
       </BrowserRouter>
     </div>
@@ -23,32 +19,3 @@ function App() {
 }
 
 export default withRouter(App);
-
-
-//React - library or framework ? create react app ? 
-
-//Why react ? - 
-/*
-1) Easy to learn : Good amount of resources and materials. Lower learning curve.
-2) Resusable components : DRY - Less lines of code.
-3) High Performance : Virtual dom in react wich make it fast. DOM - change updates(render) everything.
-4) SEO - Search engine optimisation : SEO friendly.
-5) JEST - React si easy to test.
-
-Disadvantages : 
-1)The high pace of development : Class based, es6, Hooks.
-2) Poor documentation : 
-3) View(of MVC) : React work in view part and nothing else.
-
-npm install
-npm start
-
-<BrowserRouter>
-				<Switch>
-					<Route path='/book-interaction'>
-						<BookSession />
-					</Route>
-
-        
-
-*/
--- src/Components/portfolio/PortfolioWrapper.js
+++ src/Components/portfolio/PortfolioWrapper.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Footer from "./Footer";
-import Header from "./Header";
 import Body from "./Body";
 import { withRouter } from "react-router-dom";
 
 function PortfolioWrapper() {
   return (<div>
-    <Header />
     <Body />
-    <Footer />
   </div>)
 }
 
